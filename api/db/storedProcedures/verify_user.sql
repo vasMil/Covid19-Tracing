b10c7e2c8009da60bde424dcd5d55d645b7ea3a9
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-11 11:19:44 +0200

    Creates verify_user, refactors db folder

---
+++ api/db/storedProcedures/verify_user.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS verify_user;
+DELIMITER $
+CREATE PROCEDURE verify_user (IN username VARCHAR(45), IN password VARCHAR(45))
+BEGIN
+	DECLARE userId INT DEFAULT NULL;
+    DECLARE isAdmin BOOL DEFAULT FALSE;
+    
+	SELECT user_table.user_id INTO userId
+    FROM user_table
+    WHERE user_table.username = username AND user_table.password = password;
+	
+    SELECT COUNT(*) > 0 INTO isAdmin
+    FROM admin_table
+    WHERE admin_table.admin_id = userId;
+    
+    IF userId IS NOT NULL THEN
+		SELECT userId AS "id", isAdmin AS "isAdmin", username AS "username";
+    END IF;
+END $
+DELIMITER ;
+
+CALL verify_user("mg", "pass123");
