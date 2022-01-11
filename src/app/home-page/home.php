6cd800320c3ec14e3b4997e4e40f082599994249
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-11 18:43:45 +0200

    Initializes directory for home-page

---
+++ src/app/home-page/home.php
@@ -0,0 +1,33 @@
+<?php
+    define('ROOT_PATH', dirname(__DIR__));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <!-- TODO: ADD MORE METADATA -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Covid-19 Tracing</title>
+    <!-- Include Bootstrap -->
+    <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+    <!-- Include Fontawesome -->
+    <link rel="stylesheet" href="../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
+    <!-- Include page local stylesheet -->
+    <link rel="stylesheet" href="home.min.css">
+    <!-- Include the required stylesheets for the partials used in the current page -->
+    <link rel="stylesheet" href="../php_partials/navbar/navbar.min.css">
+</head>
+<body>
+    <div class="container">
+        <?php include("../php_partials/navbar/navbar.php") ?>
+        <script src="../php_partials/navbar/navbar.js"></script>
+
+        <header id="home-header">
+            Home
+        </header>
+    </div>
+</body>
+</html>
