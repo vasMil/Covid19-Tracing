406c6208d2ecba78859064dfaec3ef23560920d7
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-11 21:02:51 +0200

    Changes navbar's z-index
    Leaflet map had a z-index vlaue of 1000 and thus was on top of the
    navbar.

--- src/app/php_partials/navbar/navbar.php
+++ src/app/php_partials/navbar/navbar.php
@@ -1,17 +1,19 @@
-<div class="navbar-flex">
-    <div class="app-title">
-        <span class="highlight-primary">Covid-19</span> Tracing
+<div class="custom-navbar">
+    <div class="navbar-flex">
+        <div class="app-title">
+            <span class="highlight-primary">Covid-19</span> Tracing
+        </div>
+        <button class="btn-navbar">
+            <i class="fas fa-bars"></i>
+        </button>
     </div>
-    <button class="btn-navbar">
-        <i class="fas fa-bars"></i>
-    </button>
-</div>
 
 
-<div class="navbar-full">
-    <div class="nav-item nav-home">Home</div>
-    <div class="nav-item nav-reg-loc">Register Location</div>
-    <div class="nav-item nav-rep-case">Report Covid Case</div>
-    <div class="nav-item nav-exposed">Have I been exposed to Covid-19?</div>
-    <div class="nav-item nav-profile">Profile</div>
+    <div class="navbar-full">
+        <div class="nav-item nav-home">Home</div>
+        <div class="nav-item nav-reg-loc">Register Location</div>
+        <div class="nav-item nav-rep-case">Report Covid Case</div>
+        <div class="nav-item nav-exposed">Have I been exposed to Covid-19?</div>
+        <div class="nav-item nav-profile">Profile</div>
+    </div>
 </div>
