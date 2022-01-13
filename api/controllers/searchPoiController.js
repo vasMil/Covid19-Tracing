8b3172b74e33eba759652ffe4d40e2d4f3014bb9
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-13 19:03:28 +0200

    Updates api so it also returns popular times

--- api/controllers/searchPoiController.js
+++ api/controllers/searchPoiController.js
@@ -3,7 +3,15 @@ const db = require('../db/connect').promise();
 exports.searchPoi = async (req, res, next) => {
     // TODO: check if search is ok
     try {
-        const rows = await db.execute(`SELECT * FROM poi_table WHERE poi_table.poi_type LIKE "%${req.query.type}%";`);
+        const rows = await db.execute(
+            `SELECT poi_table.id, poi_table.name, poi_table.address, poi_table.latitude,
+            poi_table.longitude, poi_table.rating, poi_table.rating_n, poi_table.poi_type,
+            popular_times_table.number_of_people
+            FROM poi_table
+            INNER JOIN popular_times_table ON poi_table.id = popular_times_table.poi_id
+            WHERE poi_table.poi_type LIKE "%${req.query.type}%"
+            AND popular_times_table.day = ${req.query.day}
+            AND popular_times_table.hour = ${req.query.hour};`);
 
         if (!rows[0], !rows[0][0]) return res.status(200).json({ rows: null, message: "Poi Not Found" });
 
