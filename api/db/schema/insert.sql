199e8c0e8f08035109f8d5843258397131405c2a
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-20 18:17:00 +0200

    Implements stored, returns pois where user was exposed to the virus

--- api/db/schema/insert.sql
+++ api/db/schema/insert.sql
@@ -4,10 +4,23 @@ INSERT INTO user_table (user_id, username, email, password) VALUES
 (1, 'admin', 'admin@admin.com', '$2a$04$chq0zRMXDOuQJ1./tXVpO.W4W83QuVRr8QoTuyLFOkS4Wj6TKCNsa'); # Admin123@
 
 INSERT INTO user_table (username, email, password) VALUES
-('mg', '@mail', '$2a$04$3jNJI/YsnhkLLirSXnxND.oSJ8rAnmUDwPoL3fmDGjYQ/lhdj3RHS'); # Pass123@
+('mg', '@mail', '$2a$04$3jNJI/YsnhkLLirSXnxND.oSJ8rAnmUDwPoL3fmDGjYQ/lhdj3RHS'), # Pass123@
+('test', 'test@test.com', '$2a$04$YoxIt8eQm2.CPFDCBK15JOBJp4s22eKFdaNKn6OhlM49lkHbU3q4G'), # Test123@
+('temp', 'temp@temp.com', '$2a$04$4OMqhjfu.eCGzFUcyp14AubLrXjw5fbOH8bP2mAWgYAxcm9ztFp3C'); # Temp123@
 
 # You may use this: https://www.devglan.com/online-tools/bcrypt-hash-generator
 # to hash passwords, when not using the api
 
 INSERT INTO admin_table VALUES 
 (1);
+
+INSERT INTO visit_table (user_id, poi_id, estimation, timestamp) VALUES
+(2,'ChIJ_cM58sE3XhMRCHVgkJkjUeQ',10,'2022-01-18 10:00:00'),
+(3,'ChIJ_cM58sE3XhMRCHVgkJkjUeQ',11,'2022-01-18 08:00:00'),
+(4,'ChIJ_cM58sE3XhMRCHVgkJkjUeQ',10,'2022-01-18 09:00:00'),
+(4,'ChIJ0a4YHeBJXhMRgESyORq9JSo',2,'2022-01-18 10:00:00'),
+(3,'ChIJ0a4YHeBJXhMRgESyORq9JSo',2,'2022-01-18 10:00:00');
+
+INSERT INTO covid_case_table (user_id, date) VALUES
+(3, "2022-01-17"),
+(4, "2022-01-20");
---
+++ api/db/storedProcedures/getVisits_ExposedUserToCovid.sql
@@ -0,0 +1,32 @@
+DROP PROCEDURE IF EXISTS getVisits_ExposedUserToCovid;
+DELIMITER $
+CREATE PROCEDURE getVisits_ExposedUserToCovid(IN id INT)
+BEGIN
+	SELECT poi.name AS poiName, poi.address AS poiAddress, poi.latitude AS poiLat, poi.longitude AS poiLng, visit.timestamp AS visitTimestamp
+    FROM visit_table as visit
+    INNER JOIN (SELECT covidVisit.poi_id AS covidPoiId, covidVisit.timestamp AS covidVisitTimestamp
+                FROM visit_table AS covidVisit
+                INNER JOIN covid_case_table AS covidCase ON covidCase.user_id = covidVisit.user_id
+                WHERE ABS(DATEDIFF(covidCase.date, covidVisit.timestamp)) <= 7
+                AND covidVisit.user_id != id) AS covidPois ON covidPois.covidPoiId = visit.poi_id
+	INNER JOIN poi_table AS poi ON poi.id = visit.poi_id
+    WHERE visit.user_id = id
+    AND DATEDIFF(NOW(), visit.timestamp) <= 7 
+    AND ABS(TIMESTAMPDIFF(minute, covidPois.covidVisitTimestamp, visit.timestamp)) <= 2*60
+    GROUP BY poi.id;
+END $
+DELIMITER ;
+
+-- Test
+-- CALL getVisits_ExposedUserToCovid(4);
+
+
+-- 	SELECT poi.name AS poiName, poi.address AS poiAddress, poi.latitude AS poiLat, poi.longitude AS poiLng, visit.timestamp AS visitTimestamp
+--     FROM poi_table AS poi
+--     INNER JOIN visit_table AS visit ON visit.poi_id = poi.id
+--     WHERE visit.user_id = id AND TIMESTAMPDIFF(day, now, visit.timestamp) <= 7;
+--     
+--     SELECT visit.poi_id, visit.timestamp
+--     FROM visit_table AS visit
+--     INNER JOIN covid_case_table AS covid_case ON covid_case.user_id = visit.user_id
+--     WHERE DATEDIFF(covid_case.date, CAST(visit.timestamp AS DATE)) <= 7;
