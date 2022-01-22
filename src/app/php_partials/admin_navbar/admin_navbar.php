ec67fe3c7f0dc1bd8f5908eaa65e5b6313b64b78
Vasilis Milionis <v_milionis@yahoo.com>
2022-01-22 02:02:10 +0200

    Implements navbar for admin

---
+++ src/app/php_partials/admin_navbar/admin_navbar.php
@@ -0,0 +1,11 @@
+<div class="custom-navbar">
+    <div class="logo">
+        <span class="highlight-primary">Covid-19</span>
+        Tracing
+    </div>
+    <div class="right-navbar-wrapper">
+        <div class="nav-crud-operations"><a href="#">Crud Operations</a></div>
+        <div class="nav-statistics"><a href="#">Statistics</a></div>
+    </div>
+</div>
+<link rel="stylesheet" href="./admin_navbar.min.css">
