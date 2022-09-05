b47ef942dc2e4001be7620592cd4104bace6beb5
vasMil <87028699+vasMil@users.noreply.github.com>
2022-09-05 17:31:04 +0300

    Adds user-profile route to the api

---
+++ api/controllers/userProfileController.js
@@ -0,0 +1,33 @@
+const db = require("../db/connect").promise();
+
+exports.getProfileInfo = async (req, res, next) => {
+    let info = {
+        "username": req.locals.verifiedUser.username,
+        "email": "",
+        "visitied_pois": [],
+        "was_positive_covid": []
+    };
+    try {
+        const [rows] = await db.execute(`CALL getProfileInfo(${req.locals.verifiedUser.id})`);
+        if (!rows || !rows[0] || !rows[1] || !rows[2]) {
+            res.status(500).json({
+                message: "Something went wrong!"
+            });
+            return;
+        }
+        if(!rows[0][0]) {
+            res.status(500).json({
+                message: "Something went wrong! Database returned no email!"
+            });
+            return;            
+        }
+        info.email = rows[0][0].email;
+        info.visitied_pois = rows[1];
+        info.was_positive_covid = rows[2];
+        
+        return res.status(200).json(info);
+    }
+    catch(err) {
+        next(err);
+    }
+}
--- api/protectedUserRoutes.js
+++ api/protectedUserRoutes.js
@@ -2,9 +2,11 @@ const protectedUserRouter = require('express').Router();
 const { searchPoi } = require('./controllers/searchPoiController');
 const { reportCase } = require('./controllers/reportCaseController');
 const { getPoisWhereExposedToCovid } = require('./controllers/exposureController');
+const { getProfileInfo } = require('./controllers/userProfileController');
 
 protectedUserRouter.get('/pois/search/', searchPoi);
 protectedUserRouter.post('/report-case/', reportCase);
 protectedUserRouter.get('/pois-exposed-user-to-covid/', getPoisWhereExposedToCovid);
+protectedUserRouter.get('/user-profile/', getProfileInfo);
 
 module.exports = protectedUserRouter;
