bab45ab4726c9b208a2f8ba2ef32195a2a93eb73
vasMil <87028699+vasMil@users.noreply.github.com>
2022-09-15 13:05:02 +0300

    Configures html and css layout for DispStats

--- src/app/php_partials/admin_navbar/admin_navbar.php
+++ src/app/php_partials/admin_navbar/admin_navbar.php
@@ -4,7 +4,7 @@
         Tracing
     </div>
     <div class="right-navbar-wrapper">
-        <div class="nav-crud-operations"><a href="#">Crud Operations</a></div>
-        <div class="nav-statistics"><a href="#">Statistics</a></div>
+        <div class="nav-crud-operations"><a href="../crud-operations-page/crud-operations.php">Crud Operations</a></div>
+        <div class="nav-statistics"><a href="../admin_display_stats/dispStats.php">Statistics</a></div>
     </div>
 </div>
