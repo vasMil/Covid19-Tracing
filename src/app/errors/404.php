cc4cf5ccb5bba24d8ca0929bdb1b1503347487e5
vasMil <87028699+vasMil@users.noreply.github.com>
2022-09-18 18:35:23 +0300

    Adds htaccess

---
+++ src/app/errors/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="../../../resources/EllipseGraphic.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include Fontawesome -->
+    <link rel="stylesheet" href="../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
+    <!-- Include Bootstrap -->
+    <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+    <!-- Include the required stylesheets for the partials used in the current page -->
+    <link rel="stylesheet" href="../php_partials/ellipses/ellipses.min.css">
+    <link rel="stylesheet" href="../php_partials/navbar/navbar.min.css">
+    <!-- Include page local stylesheet -->
+    <link rel="stylesheet" href="../errors/errors.min.css">
+    <title>Covid-19 Tracing</title>
+</head>
+<body>
+    <?php include("../php_partials/navbar/navbar.php") ?>
+    <script src="../php_partials/navbar/navbar.js"></script>
+    <div class="container">
+        <div class="error-message">404 Not Found</div>
+        <?php include('../php_partials/ellipses/ellipses.php'); ?>
+    </div>
+</body>
+</html>
